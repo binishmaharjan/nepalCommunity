7689383bb960d8b27b6217c7f1e334bbef4d3815
Binish Maharjan <legendbinish124@gmail.com>
2019-01-21 14:09:19 +0900

    create profile

---
+++ NepalCommunity/View/Cells/TableCell/ProfileUserCell/NCProfileUserDetailCell.swift
@@ -0,0 +1,9 @@
+//
+//  NCProfileUserDetailCell.swift
+//  NepalCommunity
+//
+//  Created by guest on 2019/01/18.
+//  Copyright © 2019年 guest. All rights reserved.
+//
+
+import Foundation
---
+++ NepalCommunity/View/Profile/NCProfileController.swift
@@ -0,0 +1,9 @@
+//
+//  NCProfileController.swift
+//  NepalCommunity
+//
+//  Created by guest on 2019/01/18.
+//  Copyright © 2019年 guest. All rights reserved.
+//
+
+import Foundation
